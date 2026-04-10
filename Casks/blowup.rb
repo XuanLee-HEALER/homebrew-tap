d6e7d18e55d09b626a8d6969222938706d5e0d20
Mouse.Lee <59839660+XuanLee-HEALER@users.noreply.github.com>
2026-04-10 06:51:32 +0000

    Update blowup to 2.0.7

--- Casks/blowup.rb
+++ Casks/blowup.rb
@@ -1,6 +1,6 @@
 cask "blowup" do
-  version "2.0.6"
-  sha256 "0aeacd01e912e3ebdfcbf10df1fe212705a0b08d28c7ea415fa5d7a70b415c95"
+  version "2.0.7"
+  sha256 "55808aed8b930bfec571e8fa37bc46d29c201c38bb7a44a5e530e9fb0396a2d0"
 
   url "https://github.com/XuanLee-HEALER/blowup/releases/download/v#{version}/blowup_#{version}_aarch64.dmg"
   name "blowup"
